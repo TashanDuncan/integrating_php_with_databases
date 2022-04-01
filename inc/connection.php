a4fe67319e1d66812a30904509b3ce262bf5a609
viewtifult <tashanduncan1994@gmail.com>
2022-04-01 21:38:34 +0100

    fetch data from database

--- inc/connection.php
+++ inc/connection.php
@@ -1,12 +1,19 @@
 <?php
 
-try{
-$db = new PDO("mysql:host=localhost;dbname=database;port=3306","root","root");
-    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-} catch(Exception $e) {
+try {
+    $db = new PDO("mysql:host=localhost;dbname=database;port=3306", "root", "root");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
     echo "Unable to Connect";
     echo $e->getMessage();
     exit;
 }
 
-echo "Connected to Database";
+try {
+    $results = $db->query("SELECT title, category, img FROM Media");
+} catch (Exception $e) {
+    echo $e->getMessage();
+    exit;
+}
+
+var_dump($results->fetchAll(PDO::FETCH_ASSOC));
