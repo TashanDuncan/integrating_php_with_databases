1d1ee955fa00ca49731107daf803d645e333e8d6
viewtifult <tashanduncan1994@gmail.com>
2022-04-01 16:17:21 +0100

    connect to database with error handling

---
+++ inc/connection.php
@@ -0,0 +1,12 @@
+<?php
+
+try{
+$db = new PDO("mysql:host=localhost;dbname=database;port=3306","root","root");
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+} catch(Exception $e) {
+    echo "Unable to Connect";
+    echo $e->getMessage();
+    exit;
+}
+
+echo "Connected to Database";
